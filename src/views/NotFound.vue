e3b8edb2ad0d734fd51d528a2aa8c1420805e0cb
Hua Thai Hung <huathaihung2001@gmail.com>
2023-04-24 14:02:06 +0700

    finish ver 2.0.1

--- src/views/NotFound.vue
+++ src/views/NotFound.vue
@@ -1,8 +1,8 @@
 <template>
     <div class="page">
         <p>
-            Oops, không thể tìm thấy trang. Trở về
-            <router-link to="/">trang chủ.</router-link>
+            OOPSSSSS, KHÔNG TÌM THẤY TRANG NÀY, TRỞ VỀ
+            <router-link to="/">TRANG CHỦ.</router-link>
         </p>
     </div>
 </template>
